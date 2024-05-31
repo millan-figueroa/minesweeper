b142c7295cbdddfcec443b39a490f393057ac36a
millan figueroa <millan.fig@gmail.com>
2024-05-31 15:55:58 -0700

    import components, develop our parent component, set fixed / updateable state values

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,1 +1,25 @@
 //Where we develop our parent component
+
+import './Game.css';
+import { useState, useEffect } from 'react';
+import GameInfo from './components/GameInfo';
+import Board from './components/Board';
+import {
+  filterBoard,
+  traverseBoard,
+  createEmptyArray,
+  plantMines,
+  getNeighborMines
+} from './components/utilities';
+
+const Game = () => {
+  //set fixed state values, these could be made into options later
+  const [height] = useState(8);
+  const [width] = useState(8);
+  const [mines] = useState(10);
+  //set updateable state values
+  const [gameData, setGameData] = useState([]);
+  const [gameStatus, setGameStatus] = useState("Game in Progress");
+  const [mineCount, setMineCount] = useState(mines);
+  //key value is used to force the game board to reset
+  const [key, setKey] = useState(false); 
